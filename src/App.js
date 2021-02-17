acf0ab6c1b3a42f2e9852863c69e3a0c923ded2e
Hevger Ibrahim <hevger@iibrahim.me>
2021-02-17 18:58:11 +0100

    Login / Signup component add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Login /> */}
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/auth/Login.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FormGroup } from "react-bootstrap";
+import { Form, Button, Card } from "react-bootstrap";
+
+export default function Login() {
+  return (
+    <Card
+      style={{ boxShadow: "0px 0px 57px -24px rgba(0,0,0,0.75)" }}
+      className="container mt-5"
+    >
+      <h1
+        style={{
+          textTransform: "uppercase",
+          fontSize: "25px",
+          textAlign: "center",
+          marginTop: "30px",
+          marginBottom: "20px",
+          color: "#7e7cea",
+        }}
+      >
+        Login
+      </h1>
+      <Form className="mb-5">
+        <FormGroup>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </FormGroup>
+
+        <FormGroup>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+          />
+        </FormGroup>
+        <p style={{ marginTop: "-10px", textAlign: "right", fontSize: "12px" }}>
+          Doesn't have an account? Sign up <a href="#">here</a>
+        </p>
+        <div className="col text-center">
+          <Button
+            style={{ background: "#7e7cea", border: 0 }}
+            variant="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </Card>
+  );
+}
